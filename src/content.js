a72c9e14880aa5842ce1e57cf8273ebd7df490c7
ZHAO Xudong <zxdong@gmail.com>
2019-03-25 10:49:42 +0800

    Add X User-agent

--- src/content.js
+++ src/content.js
@@ -5,7 +5,7 @@
 
 import createApp from 'ringcentral-embeddable-extension-common/src/spa/init'
 import * as config from './config'
-import {ringCentralConfigs} from 'ringcentral-embeddable-extension-common/src/common/app-config'
+import {ringCentralConfigs, thirdPartyConfigs} from 'ringcentral-embeddable-extension-common/src/common/app-config'
 import 'ringcentral-embeddable-extension-common/src/spa/style.styl'
 import './custom.styl'
 
@@ -16,14 +16,14 @@ let {
 
 let appConfigQuery = ''
 if (clientID || appServer) {
-  appConfigQuery = `&appKey=${clientID}&appServer=${encodeURIComponent(appServer)}`
+  appConfigQuery = `?userAgent=${thirdPartyConfigs.serviceName}_extension&disableActiveCallControl=false&appKey=${clientID}&appServer=${encodeURIComponent(appServer)}`
 }
 
 /* eslint-disable-next-line */
 ;(function() {
   console.log('import RingCentral Embeddable Voice to web page')
   var rcs = document.createElement('script')
-  rcs.src = 'https://ringcentral.github.io/ringcentral-embeddable/adapter.js?disableActiveCallControl=false' + appConfigQuery
+  rcs.src = 'https://ringcentral.github.io/ringcentral-embeddable/adapter.js' + appConfigQuery
   var rcs0 = document.getElementsByTagName('script')[0]
   rcs0.parentNode.insertBefore(rcs, rcs0)
 })()
